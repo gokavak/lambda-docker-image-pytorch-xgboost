36a20c1ddc411a73e031e9e5d03ca58bf9d76a4d
TremaMiguel <armando.trejo.marrufo@gmail.com>
2020-12-15 16:24:06 -0600

    Feat: Two step deployment pipeline

--- pipeline.sh
+++ pipeline.sh
@@ -13,7 +13,7 @@ region=${region:-us-west-2} # region defaults to us-west-2 if not defined
 DOCKER_REGISTRY="${account}.dkr.ecr.${region}.amazonaws.com"
 
 
-while getopts ":s:p:rd" OPTION
+while getopts ":s:rd" OPTION
 do
 	case $OPTION in
         s)
@@ -24,25 +24,17 @@ do
 			echo "Deploying CFN Template in stage ${stage}"
             sam deploy -t registry.yaml --stack-name ${TEMPLATE_REGISTRY} --capabilities ${CAPABILITIES} --parameter-overrides Project=${TEMPLATE_REGISTRY} Stage=${stage} User=${user} --force-upload --no-confirm-changeset
 			;;
-		p)
-            path=$OPTARG
-            path=${path:-.} # path defaults to current path if not defined
-		    echo "Retrieve Repository Name"
-            export APP_NAME=$(aws cloudformation --region "${region}" describe-stacks --stack-name "$TEMPLATE_REGISTRY" --query "Stacks[0].Outputs[1].OutputValue")
-            echo "$APP_NAME"
-            echo "Logging to ECR"
-            aws ecr get-login-password | docker login --username AWS --password-stdin ${DOCKER_REGISTRY}
-            docker build -t $DOCKER_REGISTRY/${APP_NAME//\"}:latest ${path}
-            docker push $DOCKER_REGISTRY/${APP_NAME//\"}:latest
-			;;
         d)
+            path=$OPTARG
+            path=${path:-.}
             echo "Retrieve Repository Name"
             export APP_NAME=$(aws cloudformation --region "${region}" describe-stacks --stack-name "$TEMPLATE_REGISTRY" --query "Stacks[0].Outputs[1].OutputValue")
             echo "Retrieve Bucket Name"
             export BUCKET=$(aws cloudformation --region "${region}" describe-stacks --stack-name "$TEMPLATE_REGISTRY" --query "Stacks[0].Outputs[0].OutputValue")
-            echo "$APP_NAME"
             echo "Building template"
-            sam deploy -t template.yaml --stack-name ${TEMPLATE_LAMBDA} --capabilities ${CAPABILITIES} --parameter-overrides Project=${TEMPLATE_REGISTRY} Repository=${APP_NAME//\"} Stage=${stage} --s3-bucket ${BUCKET//\"} --s3-prefix ${PREFIX_LAMBDA} --force-upload --no-confirm-changeset
+            sam build -t template.yaml
+            echo "Deploying template"
+            sam deploy --stack-name ${TEMPLATE_LAMBDA} --capabilities "CAPABILITY_NAMED_IAM" --parameter-overrides Project="${TEMPLATE_LAMBDA}" Stage=${stage} DockerPath=${path} --s3-bucket ${BUCKET//\"} --s3-prefix ${PREFIX_LAMBDA} --region "${region}" --image-repository "${DOCKER_REGISTRY}/${APP_NAME//\"}" --force-upload --no-confirm-changeset
             ;;
         \?)
             echo "Usage: pipeline.sh [-s] <stage> [-r] [-p] <path> [-d]"
