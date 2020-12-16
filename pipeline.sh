6182e7aa36a71f9211e7172bcf512e55e3df85e4
Leopoldo Corona <leopoldocorona@Leopoldos-MacBook-Pro-2.local>
2020-12-15 21:44:27 -0600

    Fix getopts to accept parameter in d option

--- pipeline.sh
+++ pipeline.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Remember to export the following ENV variables:
-# export CAPABILITIES="CAPABILITY_NAMED_IAM"
-# export PREFIX_LAMBDA="sam_templates/pytorch-lambda"
-# export TEMPLATE_LAMBDA="pytorch-lambda"
-# export TEMPLATE_REGISTRY="pytorch-example"
+export CAPABILITIES="CAPABILITY_NAMED_IAM"
+export PREFIX_LAMBDA="sam_templates/pytorch-lambda-l"
+export TEMPLATE_LAMBDA="pytorch-lambda-l"
+export TEMPLATE_REGISTRY="pytorch-example-l"
 
 account=$(aws sts get-caller-identity --query Account --output text)
 user=$(aws iam get-user --query User.UserName --output text)
@@ -13,7 +13,7 @@ region=${region:-us-west-2} # region defaults to us-west-2 if not defined
 DOCKER_REGISTRY="${account}.dkr.ecr.${region}.amazonaws.com"
 
 
-while getopts ":s::rd" OPTION
+while getopts ":s:rd:" OPTION
 do
 	case $OPTION in
         s)
@@ -32,7 +32,7 @@ do
             echo "Retrieve Bucket Name"
             export BUCKET=$(aws cloudformation --region "${region}" describe-stacks --stack-name "$TEMPLATE_REGISTRY" --query "Stacks[0].Outputs[0].OutputValue")
             echo "Building template"
-            sam build -t template.yaml --parameter-overrides DockerPath=${path}
+            sam build -t template.yaml --parameter-overrides DockerPath="${path}"
             echo "Deploying template"
             sam deploy --stack-name ${TEMPLATE_LAMBDA} --capabilities "CAPABILITY_NAMED_IAM" --parameter-overrides Project="${TEMPLATE_LAMBDA}" Stage=${stage} --s3-bucket ${BUCKET//\"} --s3-prefix ${PREFIX_LAMBDA} --region "${region}" --image-repository "${DOCKER_REGISTRY}/${APP_NAME//\"}" --force-upload --no-confirm-changeset
             ;;
